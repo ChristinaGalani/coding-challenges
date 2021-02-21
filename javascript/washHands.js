1a6382ebfffd3ac4302021ef421a4c2ec89b33be
Christina Galani <>
2021-02-21 22:35:00 +0100

    working solution

---
+++ javascript/washHands.js
@@ -0,0 +1,11 @@
+const washHands = (N, nM) => {
+  const properHandWashing = 21;
+  const dailyHandWashing = N * properHandWashing;
+  const monthlyHandWashing = dailyHandWashing * nM * 30;
+  const min = Math.floor(monthlyHandWashing / 60);
+  const sec = monthlyHandWashing % 60;
+  const totalHandWashingTime = `${min} minutes and ${sec} seconds`;
+  return totalHandWashingTime;
+};
+
+console.log(washHands(8, 7));
